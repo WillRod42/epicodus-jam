330386e9565e2edcf2fd462d16183cd0021f5e61
Sploob <crayiphone@gmail.com>
2022-06-13 16:13:27 -0700

    add killzone which resawns the character at scene 0

---
+++ Scripts/Killzone.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Killzone : MonoBehaviour
+{
+    public int Respawn;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.GetComponent<CharacterController>())
+        {
+            SceneManager.LoadScene(Respawn);
+        }
+    }
+}
