d5239e734649d600c76edc28fc086acb5bd4f83b
Sploob <crayiphone@gmail.com>
2022-06-15 10:55:53 -0700

    add healthbar

--- Scripts/LifeAndDeath.cs
+++ Scripts/LifeAndDeath.cs
@@ -7,12 +7,15 @@ public class LifeAndDeath : MonoBehaviour
   public int maxHealth;
   public AudioSource destroySound;
   public AudioSource hitSound;
+  public HealthBar healthBar;
   private int currentHealth;
   private bool destroySoundPlayed = false;
+
   // Start is called before the first frame update
   void Start()
   {
     currentHealth = maxHealth;
+    healthBar.SetMaxHealth(maxHealth);
   }
 
   // Update is called once per frame
@@ -44,6 +47,8 @@ public class LifeAndDeath : MonoBehaviour
     }
     currentHealth -= damage;
     Debug.Log(currentHealth + "/" + maxHealth);
+
+    healthBar.SetHealth(currentHealth);
   }
   public bool healthCheck()
   {
