d3ea76162ffbf5d46318b5af36256baef7851d4e
JeremyM45 <jeremyvs1945@gmail.com>
2022-06-15 09:48:40 -0700

    add sprite flip to enemyAI

--- Scripts/EnemyAi.cs
+++ Scripts/EnemyAi.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,17 @@ public class EnemyAi : MonoBehaviour
 {
   public float changeTimer;
   public float speed;
+  public bool vertical;
   private Rigidbody2D rb;
   private float timer;
+  private SpriteRenderer sr;
   
   private int direction = 1;
     // Update is called once per frame
+    void Start()
+    {
+      sr = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
       timer -= Time.deltaTime;
@@ -22,7 +29,23 @@ public class EnemyAi : MonoBehaviour
     }
     void FixedUpdate()
     {
-      Vector2 position = new Vector2(speed * direction, 0);
-      transform.Translate(position);
+      if(!vertical)
+      {
+        if(direction < 0)
+        {
+          sr.flipX = false;
+        }
+        else if(direction > 0)
+        {
+          sr.flipX = true;
+        }
+        Vector2 position = new Vector2(speed * direction, 0);
+        transform.Translate(position);
+      }
+      if(vertical)
+      {
+        Vector2 position = new Vector2(0, speed * direction);
+        transform.Translate(position);
+      }
     }
 }
