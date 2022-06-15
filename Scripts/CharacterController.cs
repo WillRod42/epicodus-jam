4a93146d97fc4a027aa4ceabc599e88fae37a2ae
William Rodriguez <william@rodriguez1.com>
2022-06-15 13:50:22 -0700

    Add player animations

--- Scripts/CharacterController.cs
+++ Scripts/CharacterController.cs
@@ -7,25 +7,40 @@ public class CharacterController : MonoBehaviour
     public float speed;
     public float jump;
     public float fall;
-    public Animator animator;
-
     public Vector2 velocity;
+    public GameObject playerSprite;
+
     private bool bounced;
     private bool jumping;
     private bool grounded;
+    private bool facingRight;
+    private bool attacking;
+    private bool attackPressed;
+    private string currentState;
+    private float attackDelay;
     
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    private Animator animator;
 
+    const string PLAYER_IDLE = "Idle";
+    const string PLAYER_WALK = "Walk";
+    const string PLAYER_ATTACK = "Attack";
+    const string PLAYER_JUMP = "Jump";
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        sr = gameObject.GetComponent<SpriteRenderer>();
-        animator = gameObject.GetComponent<Animator>();
+        sr = playerSprite.GetComponent<SpriteRenderer>();
+        animator = playerSprite.GetComponent<Animator>();
         jumping = false;
         grounded = true;
         bounced = false;
+        facingRight = true;
+        attacking = false;
+        attackDelay = 0.5f;
+
+        ChangeAnimationState(PLAYER_IDLE);
     }
 
     void Update()
@@ -37,22 +52,17 @@ public class CharacterController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-            animator.SetBool("walking", true);
             if (Input.GetKey(KeyCode.A))
             {
                 velocity = new Vector2(-speed, 0);
-                sr.flipX = true;
+                facingRight = false;
             }
 
             if (Input.GetKey(KeyCode.D))
             {
                 velocity = new Vector2(speed, 0);
-                sr.flipX = false;
+                facingRight = true;
             }
-        }
-        else
-        {
-            animator.SetBool("walking", false);
         }
 
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
@@ -60,7 +70,10 @@ public class CharacterController : MonoBehaviour
             velocity = Vector2.zero;
         }
 
-        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y - 0.55f, transform.position.z), new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), Color.red);
+        if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)))
+        {
+            attackPressed = true;
+        }
     }
 
     void FixedUpdate()
@@ -69,10 +82,16 @@ public class CharacterController : MonoBehaviour
         {
             transform.Translate(velocity);
         }
+
         if (jumping)
         {
             rb.AddForce(transform.up * jump, ForceMode2D.Impulse);
             jumping = false;
+
+            if (!attacking)
+            {
+                ChangeAnimationState(PLAYER_JUMP);
+            }
         }
 
         if (rb.velocity.y < 0 && !grounded)
@@ -82,14 +101,63 @@ public class CharacterController : MonoBehaviour
         
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
         grounded = Physics2D.BoxCast(new Vector2(position.x, position.y - 0.55f), new Vector2(1, 0.5f), 0f, Vector2.down, 0f, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (attackPressed)
+        {
+            attackPressed = false;
+            if (!attacking)
+            {
+                attacking = true;
+                ChangeAnimationState(PLAYER_ATTACK);
+                Invoke("AttackFinished", attackDelay);
+            }
+        }
+
+        if (!attacking)
+        {
+            if (Mathf.Abs(velocity.x) > 0)
+            {
+                ChangeAnimationState(PLAYER_WALK);
+            }
+            else if (grounded)
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+        }
     }
+
     public void Bounced()
     {
         bounced = true;
         Invoke("ResetBounce", 0.2f);
     }
+
     void ResetBounce()
     {
         bounced = false;
+    }
+
+    private void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        if (facingRight)
+        {
+            newState += " Right";
+        }
+        else
+        {
+            newState += " Left";
+        }
+
+        animator.Play(newState);
+        currentState = newState;
+
+        // Debug.Log("1 - " + animator.GetCurrentAnimatorClipInfo(0)[0].clip.name);
+    }
+
+    private void AttackFinished()
+    {
+        attacking = false;
     }
 }
